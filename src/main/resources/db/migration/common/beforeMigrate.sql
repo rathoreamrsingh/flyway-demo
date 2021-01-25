cfb9266a769ee5d4a6c498f72cb25409dc4ba809
Amar <amarsingh@outlook.in>
2021-01-25 12:44:46 +0800

    Amar | Added before and after migration callback in Java and updated before migration sql script.

--- src/main/resources/db/migration/common/beforeMigrate.sql
+++ src/main/resources/db/migration/common/beforeMigrate.sql
@@ -1,2 +1,6 @@
-update users set user_name = upper(user_name)
-where user_name != upper(user_name);
+/**
+* This is an example of sql callback that runs before any kind of migration.
+* Note: Commenting because this fails if the table does not exists.
+ */
+--update users set user_name = upper(user_name)
+--where user_name != upper(user_name);
